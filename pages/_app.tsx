89fde2fd35a8997f561a394135f030c2c5c6ce15
samynaj <samuelnnj@gmail.com>
2022-05-26 11:00:16 +0100

    configure _app to have global store HOC

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import '../styles/globals.scss'
 import './_app.css'
 import type { AppProps } from 'next/app'
+import { wrapper } from '../store/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp) 
