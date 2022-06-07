6bd09c1c7116b9350c07c41fa8789ac6c0fc7c9a
kcblaq <kcblack22@gmail.com>
2022-06-07 17:41:37 +0100

    Email confirm

---
+++ additional.d.ts
@@ -0,0 +1,10 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			KEY: string
+
+		}
+	}
+} // If this file has no import/export statements (i.e. is a script) // convert it into a module by adding an empty export statement. export {}
+
+export {}
