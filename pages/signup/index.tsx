2e1b5939a62cfb5d701270b081fb631f120af5b9
samynaj <samuelnnj@gmail.com>
2022-06-17 01:30:28 +0100

    signup-frontend

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -11,31 +11,49 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Index = () => {
-    const [state, setState] = useState({})
+    const [state, setState] = useState({email: '', password: ''})
     const router = useRouter()
 
     // create random strings for the wallet address
-    const address = 'jhgfdsawertyunbvertyouyfxswertyuiopohvpoiuytreqwertfdsxcvfds'
+    const address = 'qwertyuiop'
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target
         setState(prevState => ({...prevState, [name]: value}))
     }
+
+
     const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
         console.log(state)
+        const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
+        const result = pattern.test(state.email);
+
         try {
-            const res = await axios.post('/api/signup', {
-                address: address,
-                ...state
-            })
-            if (res.status === 200) {
-                toast.success('Welcome to Akara, Login to assess your account')
-                router.push('/login')
-            }
-            console.log(res)
+          if(!state.email || !state.password) {
+            return toast.error('Email or password field is empty')
+          }
+          if(!result) {
+            return toast.error('Invalid email, check email and try again')
+          }
+          if(state.password.length < 6) {
+            return toast.error('Password is too short, choose a more secured password')
+          }
+
+
+          const res = await axios.post('/api/signup', {
+              address: address,
+              email: state.email,
+              password: state.password
+          })
+
+          if (res.status === 200) {
+              toast.success('Welcome to Akara, Login to assess your account')
+              router.push('/login')
+          }
+          console.log(res)
 
         } catch (error) {
-            toast.error('There was an error, try again')
+            toast.error(error + '')
             console.log(error)
         }
         
