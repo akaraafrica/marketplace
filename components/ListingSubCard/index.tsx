7e5e92e85d9370213256fb63694a7a6cc96919fa
kcblaq <kcblack22@gmail.com>
2022-06-21 15:13:54 +0100

    Corrected styling

--- components/ListingSubCard/index.tsx
+++ components/ListingSubCard/index.tsx
@@ -10,7 +10,6 @@ import styles from "./index.module.scss";
 function ListingSubCard() {
   return (
     <div className={styles.cont}>
-      {/* <img src={`/assets/listingsubcardimg.png`} className={styles.image} /> */}
       <NextImage
         src={`/assets/listingsubcardimg.png`}
         height="180px"
@@ -27,7 +26,7 @@ function ListingSubCard() {
               border: "1px solid green",
               fontSize: "10px",
               borderRadius: "3px",
-              padding: "5px"
+              padding: "5px",
             }}
           >
             {" "}
@@ -41,42 +40,6 @@ function ListingSubCard() {
         <button>Place a bid</button>
       </div>
     </div>
-
-    // <div className={styles.listingsubcardcon}>
-    //   <div className={styles.listingsubcard}>
-    //     <div className={styles.listingsubcardsec1} style={{
-
-    //       backgroundImage: 'url(/assets/listingsubcardimg.png)', height:"200px", width:"200px", backgroundRepeat:"no-repeat"
-    //     }}>
-
-    //     </div>
-
-    //     <div className={styles.listingsubcardsec2}>
-    //       <p className={styles.listingsubcardsec2heading}>ETH never die</p>
-    //       <div className={styles.listingsubpricesec}>
-
-    //         <span className={styles.listingcardsec2price}>1.125 ETH</span>
-
-    //         <p>3 of 2</p>
-    //       </div>
-    //       <div className={styles.listingcardsubsec2btn}>
-    //         <button>Place a bid</button>
-    //       </div>
-    //     </div>
-    //     <Box sx={{display:"flex", flexDirection:"column", paddingLeft:"10px"}}>
-    //       <Typography> ETH never die</Typography>
-    //     <Box sx={{display:"flex", justifyContent:"space-around", alignItems:"center"}}>
-    //     <NextImage src={`/assets/Avator.svg`} height={60} width={40} />
-    //     <Typography sx={{color:"green", fontSize:{xs:6,sm:8,md:12,lg:12}, borderWidth:"2px",borderStyle:"solid",borderColor:"green", borderRadius:"2px"}}>1.125 ETH </Typography>
-    //     <Typography> 2 of 3</Typography>
-
-    //     </Box>
-    //     <div className={styles.listingcardsubsec2btn}>
-    //         <button>Place a bid</button>
-    //       </div>
-    //     </Box>
-    //   </div>
-    // </div>
   );
 }
 export default ListingSubCard;
