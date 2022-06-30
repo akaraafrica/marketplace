08e5bae50b2279251b9b6fbf1dc6cf35c7a6ec6e
Ada Orajiaku <adaorajiaku@gmail.com>
2022-06-30 13:02:53 +0100

    seeding db with json content

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,22 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
 
-const { PrismaClient } = require('@prisma/client');
-
-const data = require('./seed.data.json');
-const client = new PrismaClient()
+const data = require("./seed.data.json");
+const client = new PrismaClient();
 
 async function seed() {
+  try {
+    await client.user.deleteMany();
+    console.log("Deleted records user table");
+    await client.item.deleteMany();
+    console.log("Deleted records in item table");
 
-    try {
-        await client.item.deleteMany();
-        console.log("Deleted records in item table");
+    await client.user.createMany({
+        data: data.users,
+      });
+     console.log("Added user data");
 
-        await client.item.createMany({
-            data: data.items
-        })
-        console.log("Added product data");
-    } catch (error) {
-        console.log("error seeding database ", error)
-    }
+    await client.item.createMany({
+      data: data.items,
+    });
+    console.log("Added product data");
+  } catch (error) {
+    console.log("error seeding database ", error);
+  }
 }
 
-seed()
+seed();
