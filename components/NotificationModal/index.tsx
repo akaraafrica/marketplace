4a65daf5ae8e2b3408f4cba487aa1d0088e8a522
samynaj <samuelnnj@gmail.com>
2022-07-05 12:23:25 +0100

    profile diaolog, notification dialog, mobile header

--- components/NotificationModal/index.tsx
+++ components/NotificationModal/index.tsx
@@ -7,71 +7,73 @@ import styles from "./index.module.scss";
 function NotificationModal() {
   return (
     <div className={styles.notificationModalCon}>
-      <div className={styles.tooltipiconcon}>
-        {/* <img className={styles.tooltipicon} src={toolTipIcon} /> */}
+      {/* <div className={styles.tooltipiconcon}>
+        <img className={styles.tooltipicon} src={toolTipIcon} />
         <img
           alt="notification image"
           className={styles.tooltipicon}
           src={`/assets/notificationImg.png`}
         />
-      </div>
+      </div> */}
       <div className={styles.notificationModal}>
         <div className={styles.notificationModalHead}>
           <h1>Notification</h1>
           <button>See all</button>
         </div>
-        <div className={styles.notificationmodalcontent}>
-          <div className={styles.notificstionmodalcontentseccon}>
-            <div className={styles.notificstionmodalcontentsec1}>
-              <img
-                alt="notification image"
-                src={`/assets/notificationImg.png`}
-              />
+        <div className={styles.notificationBody}>
+          <div className={styles.notificationmodalcontent}>
+            <div className={styles.notificstionmodalcontentseccon}>
+              <div className={styles.notificstionmodalcontentsec1}>
+                <img
+                  alt="notification image"
+                  src={`/assets/notificationImg.png`}
+                />
+              </div>
+              <div className={styles.notificationmodalcontentsec2}>
+                <h4>ETH received</h4>
+                <h4>0.08 ETH recived</h4>
+                <p>2 days ago</p>
+              </div>
             </div>
-            <div className={styles.notificationmodalcontentsec2}>
-              <h4>ETH received</h4>
-              <h4>0.08 ETH recived</h4>
-              <p>2 days ago</p>
-            </div>
-          </div>
-          <div className={styles.acticeNotification}>
-            <img alt="active image" src={`/assets/active.svg`} />
-          </div>
-        </div>
-        <div className={styles.notificationmodalcontent}>
-          <div className={styles.notificstionmodalcontentseccon}>
-            <div className={styles.notificstionmodalcontentsec1}>
-              <img
-                alt="notification image"
-                src={`/assets/notificationImg.png`}
-              />
-            </div>
-            <div className={styles.notificationmodalcontentsec2}>
-              <h4>ETH received</h4>
-              <h4>0.08 ETH recived</h4>
-              <p>2 days ago</p>
+            <div className={styles.acticeNotification}>
+              <img alt="active image" src={`/assets/active.svg`} />
             </div>
           </div>
-          <div className={styles.acticeNotification}>
-            <img alt="active" src={`/assets/active.svg`} />
-          </div>
-        </div>
-        <div className={styles.notificationmodalcontent}>
-          <div className={styles.notificstionmodalcontentseccon}>
-            <div className={styles.notificstionmodalcontentsec1}>
-              <img
-                alt="notification image"
-                src={`/assets/notificationImg.png`}
-              />
+          <div className={styles.notificationmodalcontent}>
+            <div className={styles.notificstionmodalcontentseccon}>
+              <div className={styles.notificstionmodalcontentsec1}>
+                <img
+                  alt="notification image"
+                  src={`/assets/notificationImg.png`}
+                />
+              </div>
+              <div className={styles.notificationmodalcontentsec2}>
+                <h4>ETH received</h4>
+                <h4>0.08 ETH recived</h4>
+                <p>2 days ago</p>
+              </div>
             </div>
-            <div className={styles.notificationmodalcontentsec2}>
-              <h4>ETH received</h4>
-              <h4>0.08 ETH recived</h4>
-              <p>2 days ago</p>
+            <div className={styles.acticeNotification}>
+              <img alt="active" src={`/assets/active.svg`} />
             </div>
           </div>
-          <div className={styles.acticeNotification}>
-            <img alt="active" src={`/assets/active.svg`} />
+          <div className={styles.notificationmodalcontent}>
+            <div className={styles.notificstionmodalcontentseccon}>
+              <div className={styles.notificstionmodalcontentsec1}>
+                <img
+                  alt="notification image"
+                  src={`/assets/notificationImg.png`}
+                />
+              </div>
+              <div className={styles.notificationmodalcontentsec2}>
+                <h4>ETH received</h4>
+                <h4>0.08 ETH recived</h4>
+                <p>2 days ago</p>
+              </div>
+            </div>
+            <div className={styles.acticeNotification}>
+              <img alt="active" src={`/assets/active.svg`} />
+            </div>
           </div>
         </div>
       </div>
