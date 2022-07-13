9e87f5443c1fd8c2d87253c79c6dd6ddccc8d5ab
samuel nnaji <samuelnnj@gmail.com>
2022-07-13 10:59:41 +0100

    general marketplace

---
+++ components/Photography2/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import ProfileCard from "../ProfileCard/index";
+import styles from "./index.module.scss";
+
+function Photography({ products }: any) {
+  const initial = products.slice(0, 8);
+  const [items, setItems] = useState([...initial]);
+
+  const fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    let start = items.length;
+    let end = items.length + 8;
+    setTimeout(() => {
+      setItems(items.concat(products.slice(start, end)));
+    }, 1500);
+  };
+
+  return (
+    <InfiniteScroll
+      dataLength={items.length}
+      next={fetchMoreData}
+      hasMore={true}
+      loader={<h6 style={{ textAlign: "center" }}>Loading...</h6>}
+    >
+      <div className={styles.allitems}>
+        {items.map((item) => (
+          <div key={item.id} className={styles.container}>
+            <ProfileCard
+              ProductImg={`${item.image}`}
+              Name={item.title}
+              Price={item.price}
+              Stock={item.stock}
+              Avatar={`${item.biddersimg}`}
+              HighestBid={item.highestbid}
+            />
+          </div>
+        ))}
+      </div>
+    </InfiniteScroll>
+  );
+}
+export default Photography;
