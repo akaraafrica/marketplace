1b816bbe4dce546b15cb533fdb814a57431e6cb7
samuel nnaji <samuelnnj@gmail.com>
2022-07-18 07:53:54 +0100

    AKM-100 create and edit single item

--- pages/api/items/index.ts
+++ pages/api/items/index.ts
@@ -20,6 +20,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       console.log(error);
     }
   }
+  if (req.method === "POST") {
+    try {
+      await prisma.item.create({
+        data: {
+          title: req.body.title,
+          description: req.body.description,
+          price: req.body.price,
+          ownerId: req.body.ownerId,
+          tokenId: req.body.tokenId,
+          published: req.body.published,
+          acceptedBid: req.body.acceptedBid,
+          openForBid: req.body.openForBid,
+          owner: req.body.owner,
+          video: req.body.video,
+        },
+      });
+      res.status(201).json({ message: "Item created" });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
 };
 
 export default handler;
