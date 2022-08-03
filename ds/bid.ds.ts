f009bf98cad661d1c943924bad240c8d3dc94cba
Sdqumar <sdqumar09@gmail.com>
2022-08-03 20:02:03 +0100

    AKM-120 fix type error

--- ds/bid.ds.ts
+++ ds/bid.ds.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
-import { IBid } from "../types/bid.interface";
 
 const baseUrl = `${process.env.NEXT_PUBLIC_DOMAIN!}/api/bid`;
 
 type route = "purchase" | "placeBid" | "acceptBid";
 
 class Bid {
-  async postData(route: route, data: IBid) {
+  async postData(route: route, data: any) {
     try {
       const res = await axios.post(baseUrl + "/" + route, data);
       return res.data;
