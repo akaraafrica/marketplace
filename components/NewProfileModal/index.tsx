3cec44a30142bc232566cd4df39a47fdf3fd7e8f
Sdqumar <sdqumar09@gmail.com>
2022-10-02 18:13:59 +0100

    AKM-213-fix-user-profile-and-dashboard-menu-and-other-routes

--- components/NewProfileModal/index.tsx
+++ components/NewProfileModal/index.tsx
@@ -39,10 +39,11 @@ const Index = ({ balance }: properties) => {
           fontSize="0.7em"
           length={2}
         />
-        <Link href={`/dashboard/${user?.id}`}>
-          <span>My Profile</span>
+        <Link href={`/profile/${user?.id}`}>
+          <span>Profile</span>
         </Link>
       </div>
+
       <div className={styles.line}></div>
       <div className={styles.list}>
         <NextImage
@@ -51,8 +52,19 @@ const Index = ({ balance }: properties) => {
           alt="user icon"
           src="/assets/collectionicon.svg"
         />
+        <Link href={`/dashboard/${user?.id}`}>
+          <span>Dashboard</span>
+        </Link>
+      </div>
+      <div className={styles.list}>
+        <NextImage
+          width="20px"
+          height="20px"
+          alt="user icon"
+          src="/assets/collectionicon.svg"
+        />
         <Link href={`/collections`}>
-          <span>My Collections</span>
+          <span>Collections</span>
         </Link>
       </div>
       <div className={styles.line}></div>
