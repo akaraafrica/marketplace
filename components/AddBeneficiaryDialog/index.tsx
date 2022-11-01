f0f2a6c9cf8f4b79438445939466a94a74291424
samuel nnaji <samuelnnj@gmail.com>
2022-11-01 23:56:29 +0100

    added ui for adding existing user as beneficiary

--- components/AddBeneficiaryDialog/index.tsx
+++ components/AddBeneficiaryDialog/index.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import { CollectionDs } from "../../ds";
 import Dialog from "../global/Dialog";
 import styles from "./styles.module.scss";
 import dynamic from "next/dynamic";
 import { toast } from "react-toastify";
+import { IUser } from "../../types/user.interface";
+import userDs from "../../ds/user.ds";
+import { AuthContext } from "../../contexts/AuthContext";
+import Image from "../Image";
+import DefaultAvatar from "../DefaultAvatar";
 const ReactQuill: any = dynamic(() => import("react-quill"), { ssr: false });
 const toolbarOptions = [
   ["bold", "italic", "underline", "strike"],
@@ -41,7 +46,13 @@ const Index: React.FC<Properties> = ({
   const [wallet, setWallet] = useState("");
   const [description, setDescription] = useState("");
   const [percent, setPercent] = useState(0);
+  const [page, setPage] = useState(0);
+  const [searchUser, setSearchUser] = useState("");
+  const [searchedUser, setSearchedUser] = useState<IUser[]>([]);
+  const [selectedUser, setSelectedUser] = useState<IUser[]>([]);
+  const [resultDisplay, setResultDisplay] = useState(false);
 
+  const { user } = useContext(AuthContext);
   useEffect(() => {
     if (beneficiary) {
       setName(beneficiary.name);
@@ -51,6 +62,22 @@ const Index: React.FC<Properties> = ({
       setPercent(beneficiary.percentage);
     }
   }, [beneficiary]);
+
+  let queryCall = useRef<any>();
+
+  useEffect(() => {
+    clearTimeout(queryCall.current);
+
+    const fetchData = async () => {
+      const data = await userDs.fetchSearchedUsers(searchUser);
+      setSearchedUser(data.searchedUsersWithoutPassword);
+    };
+    if (!searchUser) return;
+
+    queryCall.current = setTimeout(() => {
+      fetchData();
+    }, 250);
+  }, [searchUser]);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -119,73 +146,164 @@ const Index: React.FC<Properties> = ({
     <Dialog open={open} handleClose={handleDialogClose}>
       <main className={styles.main}>
         <h4>{beneficiary ? "Edit" : "Add"} Beneficiary</h4>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.inputDiv}>
-            <label>Name</label>
+        <div className={styles.top}>
+          <span
+            onClick={() => setPage(0)}
+            style={{ background: page === 0 ? "#f2994a" : "inherit" }}
+          >
+            Existing user
+          </span>
+          <span
+            onClick={() => setPage(1)}
+            style={{ background: page === 1 ? "#f2994a" : "inherit" }}
+          >
+            New user
+          </span>
+        </div>
+        {page === 0 && (
+          <div className={styles.itemdetailsforminputSearch}>
+            <label>SEARCH TO SELECT BENEFICIARY</label>
             <input
-              onChange={(e) => setName(e.target.value)}
-              value={name}
               type="text"
-              required
-              placeholder="John doe"
+              name="Search"
+              placeholder="Search users"
+              value={searchUser}
+              onChange={(e) => {
+                setResultDisplay(true);
+                setSearchUser(e.target.value);
+              }}
             />
+            <div
+              style={{ display: `${resultDisplay ? "flex" : "none"}` }}
+              className={styles.searchResults}
+            >
+              {searchUser &&
+                searchedUser?.map((user, index) => (
+                  <span
+                    key={index}
+                    onClick={() => {
+                      for (let i = 0; i < selectedUser.length; i++) {
+                        if (
+                          selectedUser[i].walletAddress === user.walletAddress
+                        ) {
+                          toast.warning("User already selected");
+                          return;
+                        }
+                      }
+                      setSelectedUser([...selectedUser, user]);
+                      // setItems([...items, ...userItems]);
+                      setSearchUser("");
+                      setResultDisplay(false);
+                    }}
+                  >
+                    {user.email && user.email}
+                  </span>
+                ))}
+            </div>
+            <div className={styles.itemImagesDiv}>
+              {selectedUser.map((selUser, index) => (
+                <div key={index} className={styles.userImage}>
+                  <div
+                    className={styles.closeIcon}
+                    onClick={() =>
+                      setSelectedUser([
+                        ...selectedUser.slice(0, index),
+                        ...selectedUser.slice(index + 1, selectedUser.length),
+                      ])
+                    }
+                    style={{
+                      display: selUser.id === user?.id ? "none" : "block",
+                    }}
+                  >
+                    <Image
+                      width="30px"
+                      height="30px"
+                      alt="close icon"
+                      src={`/assets/closeicon.svg`}
+                    />
+                  </div>
+                  <DefaultAvatar
+                    fontSize=".6rem"
+                    id={user!.id}
+                    url={user?.profile?.avatar}
+                    walletAddress={selUser.walletAddress}
+                    width="56px"
+                    height="56px"
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-          <div className={styles.inputDiv}>
-            <label>Email</label>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              required
-              value={email}
-              placeholder="example@gmail.com"
-            />
-          </div>
-          <div className={styles.inputDiv}>
-            <label>Wallet Address</label>
-            <input
-              onChange={(e) => setWallet(e.target.value)}
-              type="text"
-              required
-              value={wallet}
-              placeholder="0x00000000000..."
-            />
-          </div>
-          <div className={styles.inputDiv}>
-            <label>Description</label>
-            {/* <input
+        )}
+        {page === 1 && (
+          <form onSubmit={handleSubmit}>
+            <div className={styles.inputDiv}>
+              <label>Name</label>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type="text"
+                required
+                placeholder="John doe"
+              />
+            </div>
+            <div className={styles.inputDiv}>
+              <label>Email</label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                required
+                value={email}
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div className={styles.inputDiv}>
+              <label>Wallet Address</label>
+              <input
+                onChange={(e) => setWallet(e.target.value)}
+                type="text"
+                required
+                value={wallet}
+                placeholder="0x00000000000..."
+              />
+            </div>
+            <div className={styles.inputDiv}>
+              <label>Description</label>
+              {/* <input
               onChange={(e) => setDescription(e.target.value)}
               type="text"
               placeholder="Enter beneficiary description"
             /> */}
-            <ReactQuill
-              modules={{
-                toolbar: toolbarOptions,
-              }}
-              theme="snow"
-              style={{
-                height: "10rem",
-                marginBottom: "100px",
-              }}
-              placeholder='e.g. “Funds will be sent to beneficiary...”"'
-              value={description}
-              onChange={(e: any) => {
-                setDescription(e);
-              }}
-            />
-          </div>
-          <div className={styles.inputDiv}>
-            <label>Percentage</label>
-            <input
-              onChange={(e) => setPercent(parseInt(e.target.value))}
-              type="number"
-              max={100}
-              required
-              value={percent}
-              placeholder="10%"
-            />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+              <ReactQuill
+                modules={{
+                  toolbar: toolbarOptions,
+                }}
+                theme="snow"
+                style={{
+                  height: "10rem",
+                  marginBottom: "100px",
+                }}
+                placeholder='e.g. “Funds will be sent to beneficiary...”"'
+                value={description}
+                onChange={(e: any) => {
+                  setDescription(e);
+                }}
+              />
+            </div>
+            <div className={styles.inputDiv}>
+              <label>Percentage</label>
+              <input
+                onChange={(e) => setPercent(parseInt(e.target.value))}
+                type="number"
+                max={100}
+                required
+                value={percent}
+                placeholder="10%"
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
       </main>
     </Dialog>
   );
