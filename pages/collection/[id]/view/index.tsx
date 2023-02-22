9d3de61e98cd73908df49424f87d6fe4e63d0e1e
samuel nnaji <samuelnnj@gmail.com>
2023-02-22 11:03:02 +0100

    protect image download with a layer of div

---
+++ pages/collection/[id]/view/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+import Layout from "../../../../components/global/Layout";
+
+const Index = () => {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  );
+};
+
+export default Index;
